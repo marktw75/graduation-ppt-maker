9c85734fb92db2c4a7c9ea9a4df0c1e3c4f6adc6
Mark Tsai <marktw75@gmail.com>
2025-05-12 18:03:31 +0800

    Add font shadown

--- image_text_center.sh
+++ image_text_center.sh
@@ -3,8 +3,8 @@
 # image_text_center.sh - 在原始圖片上生成中間有文字的圖片
 
 # 檢查參數數量
-if [ "$#" -ne 5 ]; then
-    echo "用法: $0 '文字' <字體大小> <原始圖片路徑> <x坐標> <y坐標>"
+if [ "$#" -ne 6 ]; then
+    echo "用法: $0 '文字' <字體大小> <原始圖片路徑> <x坐標> <y坐標> <字體顏色>"
     exit 1
 fi
 
@@ -14,6 +14,7 @@ fontsize="$2"
 input_image="$3"
 x="$4"
 y="$5"
+fontcolor="$6"  # 新增參數：字體顏色
 
 # 設置預設輸出路徑
 output_dir="/home/mark/dev/graduation-ppt-maker/output"
@@ -22,8 +23,8 @@ output="$output_dir/000_front_page.png"  # 可以根據需要修改文件名
 # 確保輸出目錄存在
 mkdir -p "$output_dir"
 
-# 使用 ffmpeg 在原始圖片上添加文字，將字體顏色設置為黑色
-ffmpeg -i "$input_image" -vf "drawtext=text='$text':fontcolor=black:fontsize=$fontsize:x=$x:y=$y" -y "$output"
+# 使用 ffmpeg 在原始圖片上添加文字，模擬立體效果
+ffmpeg -i "$input_image" -vf "drawtext=text='$text':fontcolor=black:fontsize=$fontsize:x=$((x+2)):y=$((y+2)), drawtext=text='$text':fontcolor=$fontcolor:fontsize=$fontsize:x=$x:y=$y" -y "$output"
 
 if [ $? -eq 0 ]; then
     echo "已生成圖片: $output"
